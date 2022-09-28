9d811ffdde536228b3f811bf70321d4f38d838d4
Jasurbek <jasurisoqov685@gmail.com>
2022-09-28 12:10:00 +0500

    Proect html file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from "react";
 
+import "./App.css";
+
 const App = () => {
-  return (
-    <div>
-      <h1>App</h1>
-    </div>
-  );
+  return <h1 className="underline text-3xl">App</h1>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 
 import App from "./App";
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+import "./index.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
