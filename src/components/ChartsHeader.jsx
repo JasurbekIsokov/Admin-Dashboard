54c583452a9f89785141cb049b6d5b077416332c
Jasurbek <jasurisoqov685@gmail.com>
2022-09-29 07:01:00 +0500

    create fileStructure in Pages Folder

---
+++ src/components/ChartsHeader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsHeader = () => {
+  return <div>ChartsHeader</div>;
+};
+
+export default ChartsHeader;
---
+++ src/components/ThemeSettings.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const ThemeSettings = () => {
+  return <div>ThemeSettings</div>;
+}
+
+export default ThemeSettings
