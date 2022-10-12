982a79990f531886ab71f872d64e76fd7e185bab
Jasurbek <jasurisoqov685@gmail.com>
2022-10-12 19:26:31 +0500

    NavLink Link name

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,6 +9,11 @@ import { links } from "../data/dummy";
 const Sidebar = () => {
   const activeMenu = true;
 
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
+
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
@@ -32,12 +37,27 @@ const Sidebar = () => {
           </div>
           <div className="mt-10">
             {links.map((item) => (
-              <p
-                key={links.indexOf(item)}
-                className="text-gray-400 m-3 mt-4 uppercase"
-              >
-                {item.title}
-              </p>
+              <div key={item.title}>
+                <p
+                  key={links.indexOf(item)}
+                  className="text-gray-400 m-3 mt-4 uppercase"
+                >
+                  {item.title}
+                </p>
+                {item.links.map((link) => (
+                  <NavLink
+                    to={`/${link.name}`}
+                    key={link.name}
+                    onClick={() => {}}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    {link.icon}
+                    <span className="capitalize ">{link.name}</span>
+                  </NavLink>
+                ))}
+              </div>
             ))}
           </div>
         </>
