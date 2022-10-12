f7ec48a923788cb9434be15dcdc63de4bb77af47
Jasurbek <jasurisoqov685@gmail.com>
2022-10-12 21:16:35 +0500

    createContext and initialState

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,10 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
