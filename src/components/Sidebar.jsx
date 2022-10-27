e7bb514fba715bf49bf1a67957912bc98ab1b5f6
Jasurbek <jasurisoqov685@gmail.com>
2022-10-27 18:10:08 +0500

    deleted all written codes and restarted praect

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,73 +1,7 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { SiShopware } from "react-icons/si";
-import { MdOutlineCancel } from "react-icons/md";
-import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-
-import { links } from "../data/dummy";
-import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
-
-  const activeLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2";
-  const normalLink =
-    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
-
-  return (
-    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
-      {activeMenu && (
-        <>
-          <div className="flex justify-between items-center">
-            <Link
-              to="/"
-              onClick={() => setActiveMenu(false)}
-              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
-            >
-              <SiShopware /> <span>Shoppy</span>
-            </Link>
-            <TooltipComponent content="Menu" position="BottomCenter">
-              <button
-                onClick={() =>
-                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
-                }
-                type="button"
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4  block md:hidden"
-              >
-                <MdOutlineCancel />
-              </button>
-            </TooltipComponent>
-          </div>
-          <div className="mt-10">
-            {links.map((item) => (
-              <div key={item.title}>
-                <p
-                  key={links.indexOf(item)}
-                  className="text-gray-400 m-3 mt-4 uppercase"
-                >
-                  {item.title}
-                </p>
-                {item.links.map((link) => (
-                  <NavLink
-                    to={`/${link.name}`}
-                    key={link.name}
-                    onClick={() => {}}
-                    className={({ isActive }) =>
-                      isActive ? activeLink : normalLink
-                    }
-                  >
-                    {link.icon}
-                    <span className="capitalize ">{link.name}</span>
-                  </NavLink>
-                ))}
-              </div>
-            ))}
-          </div>
-        </>
-      )}
-    </div>
-  );
+  return <div>Sidebar</div>;
 };
 
 export default Sidebar;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
-import { ContextProvider } from "./contexts/ContextProvider";
 
-import "./index.css";
-
-ReactDOM.render(
-  <ContextProvider>
-    <App />
-  </ContextProvider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
