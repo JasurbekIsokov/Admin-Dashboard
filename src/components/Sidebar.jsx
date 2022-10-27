d7cb68a2b2aae7746f5f1f96ce02a000207aae2a
Jasurbek <jasurisoqov685@gmail.com>
2022-10-27 22:43:44 +0500

    links item title

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,6 +31,16 @@ const Sidebar = () => {
               </button>
             </TooltipComponent>
           </div>
+          <div className="mt-10">
+            {links.map((item) => (
+              <div
+                key={item.title}
+                className="text-gray-400 m-3 mt-4 uppercase"
+              >
+                {item.title}
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
