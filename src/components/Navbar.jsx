1fe6b38f75260d5b5a183c4efd3a48f563571f52
Jasurbek <jasurisoqov685@gmail.com>
2022-10-27 22:31:01 +0500

    SiShopware icon and Shoppy

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return <div></div>;
 };
 
 export default Navbar;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCansel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { links } from "../data/dummy";
 
 const Sidebar = () => {
-  return <div>Sidebar component</div>;
+  const activeMenu = true;
+
+  return (
+    <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <SiShopware /> <span>Shoppy</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
