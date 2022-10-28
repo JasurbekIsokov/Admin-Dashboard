122944b088fe929df6375115d2dd493904958d39
Jasurbek <jasurisoqov685@gmail.com>
2022-10-28 09:35:23 +0500

    import context provider in index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,11 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { ContextProvider } from "./contexts/ContextProvider";
+
+ReactDOM.render(
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
+  document.getElementById("root")
+);
