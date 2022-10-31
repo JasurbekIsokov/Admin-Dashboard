b261e67f509bcd05dfa57425149e0f3d0e0992b4
Jasurbek <jasurisoqov685@gmail.com>
2022-10-31 23:49:29 +0500

    button card

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -30,6 +30,31 @@ const Ecommerce = () => {
             />
           </div>
         </div>
+
+        <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
+          {earningData.map((item) => (
+            <div
+              key={item.title}
+              className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-5 p-4 pt-9 rounded-2xl"
+            >
+              <button
+                type="button"
+                style={{ color: item.iconColor, backgroundColor: item.iconBg }}
+                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+              >
+                {item.icon}
+              </button>
+
+              <p className="mt-3">
+                <span className="text-lg font-semibold">{item.amount}</span>
+                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  {item.percentage}
+                </span>
+              </p>
+              <p className="text-sm text-gray-400 mt-1">{item.title}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
