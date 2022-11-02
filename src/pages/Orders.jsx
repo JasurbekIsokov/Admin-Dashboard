72660b3c57a884c39c35353c64ead8de2ad68ac1
Jasurbek <jasurisoqov685@gmail.com>
2022-11-02 11:21:00 +0500

    Sorting element and pagination

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -19,14 +19,32 @@ import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
-      <GridComponent>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
         <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
           {ordersGrid.map((item, index) => (
             <ColumnDirective key={index} {...item} />
           ))}
         </ColumnsDirective>
+        <Inject
+          services={[
+            Resize,
+            Sort,
+            ContextMenu,
+            Filter,
+            Page,
+            ExcelExport,
+            Edit,
+            PdfExport,
+          ]}
+        />
       </GridComponent>
     </div>
   );
