f69a1cf83ee23604a79ceddcf50ea629ebd2aa74
Jasurbek <jasurisoqov685@gmail.com>
2022-11-02 10:35:29 +0500

    SparkLine chart

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ const App = () => {
           )}
 
           <div
-            className={`dark-bg-main-bg bg-main-bg min-h-screen w-full ${
+            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? "md:ml-72" : "flex-2"
             }`}
           >
--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,10 +10,7 @@ const Ecommerce = () => {
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
-        <div
-          className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44
-      rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center"
-        >
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
           <div className="flex justify-between items-center">
             <div>
               <p className="font-bold text-gray-400">Earnings</p>
@@ -30,7 +27,6 @@ const Ecommerce = () => {
             />
           </div>
         </div>
-
         <div className="flex m-3 flex-wrap justify-center gap-1 items-center">
           {earningData.map((item) => (
             <div
@@ -40,7 +36,7 @@ const Ecommerce = () => {
               <button
                 type="button"
                 style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-                className="text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl"
+                className="text-2xl opacity-0.9 rounded-full  p-4 hover:drop-shadow-xl"
               >
                 {item.icon}
               </button>
