ff8b985161b58bf5347fa8467073ba9545fc254e
Jasurbek <jasurisoqov685@gmail.com>
2022-11-02 11:04:10 +0500

    Header component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
+
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return (
+    <div>
+      <Header category="Page" title="Orders" />
+    </div>
+  );
 };
 
 export default Orders;
