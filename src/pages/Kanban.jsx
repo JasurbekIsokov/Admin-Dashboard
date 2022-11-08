06d95eb9ecb6c9f35ad59a317efb09a35c193fde
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 12:09:29 +0500

    kanban page kanban component and ColumnDirective

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -12,6 +12,19 @@ const Kanban = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ contentField: "Summary", headerField: "Id" }}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {kanbanGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   );
 };
