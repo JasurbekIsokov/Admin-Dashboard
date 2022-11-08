4a0ebca9a5b68c5b2fd1c0b74bae75ddc915acee
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 11:23:01 +0500

    Calendar page Header section

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {
+  ScheduleComponent,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragANdDrop,
+} from "@syncfusion/ej2-react-schedule";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+    </div>
+  );
 };
 
 export default Calendar;
