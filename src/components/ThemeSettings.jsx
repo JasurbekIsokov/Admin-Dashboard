a6992dbc3464321912ac30b1c4d9c8a0109f3db9
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 12:38:40 +0500

    ThemeSettings components header section paragraph and MdOutlineCancel button

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsCheck } from "react-icons/bs";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import { themeColors } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const ThemeSettings = () => {
-  return <div>ThemeSettings</div>;
-}
+  return (
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200  bg-white dark:bg-[#484B52] w-400">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
+          <button
+            type="button"
+            onClick={() => {}}
+            style={{ color: "rgb(153, 171, 180)", borderRadius: "50%" }}
+            className="text-2xl p-3 hover:drop-shadow-xl hover:bg-light-gray"
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ThemeSettings
+export default ThemeSettings;
