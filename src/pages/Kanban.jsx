4d55969f455d94c1b7eccca70a93083e76f624dc
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 12:07:59 +0500

    Kanban page header section

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Kanban = () => {
-  return <div>Kanban</div>;
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+    </div>
+  );
 };
 
 export default Kanban;
