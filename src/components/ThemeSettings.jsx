c55fdffd2b900ddddc6822bcc0259bb7eca1fe1d
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 12:44:48 +0500

    ThemeSettings component Theme Option

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -21,6 +21,42 @@ const ThemeSettings = () => {
             <MdOutlineCancel />
           </button>
         </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-xl ">Theme Option</p>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pointer"
+              onChange={() => {}}
+              checked={true}
+            />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+
+          <div className="mt-2">
+            <input
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              onChange={() => {}}
+              className="cursor-pointer"
+              checked={true}
+            />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
