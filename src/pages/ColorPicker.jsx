44e02a7fe86ad00e50f4ae2ff655c52d6d097c91
Jasurbek <jasurisoqov685@gmail.com>
2022-11-08 12:28:59 +0500

    ColorPicker page create Inline Palette and Inline Picker

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -3,13 +3,32 @@ import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 
 import { Header } from "../components";
 
+const CustomColorPicker = ({ id, mode }) => (
+  <ColorPickerComponent
+    id={id}
+    mode={mode}
+    modeSwitcher={false}
+    inline
+    showButtons={false}
+  />
+);
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Color Picker" />
       <div className="text-center">
         <div id="preview" />
-        <div className="flex justify-center items-center gap-20 flex-wrap"></div>
+        <div className="flex justify-center items-center gap-20 flex-wrap">
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <CustomColorPicker id="inline-palette" mode="Palette" />
+          </div>
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+            <CustomColorPicker id="inline-picker" mode="Picker" />
+          </div>
+        </div>
       </div>
     </div>
   );
