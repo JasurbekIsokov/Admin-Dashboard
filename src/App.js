671b640e29741f689ef225cbfec29aecb013adcc
Jasurbek <jasurisoqov685@gmail.com>
2022-11-09 15:12:37 +0500

    App backgroundcolor colorMode

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
   } = useStateContext();
 
   return (
-    <div>
+    <div className={currentMode === "Dark" ? "dark" : " "}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -47,7 +47,7 @@ const App = () => {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                style={{ background: currentColor, borderRadius: "50%" }}
                 onClick={() => setThemeSettings(true)}
               >
                 <FiSettings />
