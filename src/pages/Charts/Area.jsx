fdcdcd7fc32578f895995d2defb7c05c4ba2c041
Jasurbek <jasurisoqov685@gmail.com>
2022-11-10 23:58:00 +0500

    Area page

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -23,7 +23,24 @@ const Area = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Area" title="Inflation Rate in percentage" />
-      <div className="w-full"></div>
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {areaCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
     </div>
   );
 };
