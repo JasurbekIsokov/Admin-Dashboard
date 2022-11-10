89b6f774850ab1a885e524f984f72e10f9f9a72d
Jasurbek <jasurisoqov685@gmail.com>
2022-11-10 23:56:43 +0500

    Area page header sectoin

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  SplineAreaSeries,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+
+import { Header } from "../../components";
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Area = () => {
-  return <div>Area</div>;
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Area" title="Inflation Rate in percentage" />
+      <div className="w-full"></div>
+    </div>
+  );
 };
 
 export default Area;
