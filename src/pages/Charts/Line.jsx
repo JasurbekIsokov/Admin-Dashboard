1c6fd48ee5cdc37cf067e6d1d03d29c5a6c87b8c
Jasurbek <jasurisoqov685@gmail.com>
2022-11-10 23:46:24 +0500

    Line page and LineChart component

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -6,6 +6,9 @@ const Line = () => {
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
       <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <LineChart />
+      </div>
     </div>
   );
 };
