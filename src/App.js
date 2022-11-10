b0bb147059da5bcbf9cd9abbbba2b376d9820f64
Jasurbek <jasurisoqov685@gmail.com>
2022-11-10 22:33:05 +0500

    Line Page Header Section

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ const App = () => {
           )}
 
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? "md:ml-72" : "flex-2"
             }`}
           >
--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
+import { Header, LineChart } from "../../components";
+
 const Line = () => {
-  return <div>Line</div>;
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+    </div>
+  );
 };
 
 export default Line;
