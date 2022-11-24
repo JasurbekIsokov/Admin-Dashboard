8f94e1d69d3bfdf972843f1c16a33e42db41f660
Jasurbek <jasurisoqov685@gmail.com>
2022-11-24 11:28:10 +0500

    Button component and Cart component finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 
-const Button = ({ color, bgColor, text, borderRadius, size }) => {
+import { useStateContext } from "../contexts/ContextProvider";
+
+const Button = ({
+  icon,
+  bgColor,
+  color,
+  bgHoverColor,
+  size,
+  text,
+  borderRadius,
+  width,
+}) => {
+  const { setIsClicked, initialState } = useStateContext();
+
   return (
     <button
       type="button"
+      onClick={() => setIsClicked(initialState)}
       style={{ backgroundColor: bgColor, color, borderRadius }}
-      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      className={` text-${size} p-3 w-${width} hover:drop-shadow-xl hover:bg-${bgHoverColor}`}
     >
-      {text}
+      {icon} {text}
     </button>
   );
 };
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -55,6 +55,7 @@ export const ContextProvider = ({ children }) => {
         setThemeSettings,
         setMode,
         setColor,
+        initialState,
       }}
     >
       {children}
